6e02fc90a0658d6f880f80d45329b821f1d4ee12
Louis Guitton <louisguitton93@gmail.com>
2021-10-31 14:30:08 +0100

    add logging

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 client = Bot(command_prefix="!")
 
 
-@client.command(name="clear", help="this command will clear msgs")
+@client.event
+async def on_ready():
+    logging.info("Connected")
+    logging.info(f"Logged in as: {client.user} - {client.guilds}")
+
+
+@client.command(name="clear", help="Clears last [amount] messages in the channel")
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount + 1)
 
